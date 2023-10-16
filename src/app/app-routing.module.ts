9322aacee0e42cd3cec58f3c0ab07d1bb053f45c
vainicius <comercial.viniciusgabriel@gmail.com>
2023-10-16 20:44:05 -0300

    feat: esqueleto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TutorListComponent } from './views/tutor/tutor-list/tutor-list.component';
 import { AnimalListComponent } from './views/animal/animal-list/animal-list.component';
+import { AnimalDetailsComponent } from './views/animal/animal-details/animal-details.component';
 
 const routes: Routes = [
   {path: "tutor", component: TutorListComponent},
-  {path: "animal", component: AnimalListComponent}
+  {path: "animal", component: AnimalListComponent},
+    {path: "animal/cadastro", component: AnimalDetailsComponent}
+  
 
 ];
 
