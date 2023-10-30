d4ad38eeeffd6e19635ec397b2acae07685ad5a0
Gustavo Piegat <gustavopiegat2004@gmail.com>
2023-10-30 20:13:17 -0300

    Model de consulta e enum de status ok

--- src/app/models/consulta/consulta.ts
+++ src/app/models/consulta/consulta.ts
@@ -1,2 +1,15 @@
+import { Anamnese } from "../anamnese/anamnese";
+import { Animal } from "../animal/animal";
+import { Status } from "../enums/status";
+import { Tutor } from "../tutor/tutor";
+import { Usuario } from "../usuario/usuario";
+
 export class Consulta {
+    animal!: Animal;
+    tutor!: Tutor;
+    anamnese!: Anamnese;
+    data!: Date;
+    status!: Status;
+    veterinario!: Usuario;
+  
 }
---
+++ src/app/models/enums/status.ts
@@ -0,0 +1,6 @@
+export enum Status {
+    EM_ANDAMENTO,
+    CONCLUIDA,
+    CANCELADA,
+    AGENDADA
+}
