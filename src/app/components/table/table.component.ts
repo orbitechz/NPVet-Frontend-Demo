6e70977b3b2670a6cf8bce7422dce326af7f88a7
Jean - Autofoz <jeanfelipe0610@gmail.com>
2023-10-31 11:55:01 -0300

    feat: filtro completo

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -46,7 +46,6 @@ export class TableComponent implements OnInit {
   constructor(private http: HttpClient, private router: Router) {}
 
   async ngOnInit() {
-    this.switchEstado.setValue(true);
     await this.getAll();
     this.filter.valueChanges
       .pipe(
@@ -86,53 +85,15 @@ export class TableComponent implements OnInit {
     } catch (error) {}
     return valor;
   }
-  // ====================== SEARCH  ======================
-  // search(pesquisa?: string){
-  //   if(pesquisa){
-
-  //     this.dadosFiltrados = this.dados.filter((entidade) => {
-  //       const term = pesquisa.toLowerCase();
-  //       this.headers.forEach(header => {
-  //         let campo = header.campo.split('.');
-  //         campo.forEach(p => {
-  //           console.log(entidade[p])
-  //           return (entidade[p].includes(term))
-  //           &&
-  //           (!this.switchEstado.value || (entidade.deletedAt === null && this.switchEstado.value))
-  //         });
-  //       });
-  //     });
-  //   }else{
-  //     this.dadosFiltrados = this.dados
-  //   }
-  // }
-  // search(text?: string, pipe?: PipeTransform): any[] {
-  //   console.log(text);
-  //   if(text){
-  //     return this.dados.filter((dado) => {
-  //       const term = text.toLowerCase();
-  //       this.headers.forEach((header) => {
-  //         let campo = header.campo;
-  //         return (
-  //           dado[campo].toLowerCase().includes(term) &&
-  //           (!this.switchEstado.value ||
-  //             (dado.deletedAt === null && this.switchEstado.value))
-  //         );
-  //       });
-  //     });
-  //   }else{
-  //     return this.dados
-  //   }
-  // }
   search(text: string, pipe: PipeTransform): any[] {
+    7;
     return this.dados.filter((item) => {
       const term = text.toLowerCase();
-
       // Verifique se alguma das colunas contém o termo pesquisado
       for (const header of this.headers) {
         const campo = header.campo.split('.');
-        let entidade
-        let valor
+        let entidade;
+        let valor;
         valor = entidade = item;
 
         for (const p of campo) {
@@ -147,21 +108,24 @@ export class TableComponent implements OnInit {
           if (typeof valor != 'string') {
             formattedValue = pipe.transform(valor).toLowerCase();
           } else {
-            if(this.isData(valor)){
-              formattedValue = this.datePipe.transform(valor, "dd/MM/yyyy") as string
-            }else{
+            if (this.isData(valor)) {
+              formattedValue = this.datePipe.transform(
+                valor,
+                'dd/MM/yyyy'
+              ) as string;
+            } else {
               formattedValue = valor.toLowerCase();
             }
           }
           if (
-            pipe.transform(entidade.id).includes(term) ||
-            formattedValue.includes(term) &&
+            (pipe.transform(entidade.id).includes(term) ||
+              formattedValue.includes(term)) &&
             (!this.switchEstado.value ||
               (entidade.deletedAt === null && this.switchEstado.value))
           ) {
             return true;
           }
-        }else{
+        } else {
           return false;
         }
       }
