1f43efe705fa29e2fe747604415ee5195bacec16
Bouchra Akl <bushraakl1234@gmail.com>
2023-11-02 12:25:08 -0300

    feat : table calling and config

--- src/app/views/usuario/usuario-listar/usuario-listar.component.ts
+++ src/app/views/usuario/usuario-listar/usuario-listar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Header } from 'src/app/components/table/header';
 
 @Component({
   selector: 'app-usuario-listar',
@@ -6,4 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./usuario-listar.component.scss']
 })
 export class UsuarioListarComponent {
+  isErro!: boolean
+  mensagem!: string
+
+  // Table Configuarations
+  apiUrlPath(){
+    return 'http://localhost:8080/usuario';  
+  }
+  callHeaders(){
+    let tableHeaders : Header[] = [];
+    tableHeaders.push(new Header('Nome', 'nome'));
+    tableHeaders.push(new Header('Username', 'username')); 
+    tableHeaders.push(new Header('CPF', 'cpf'));
+    tableHeaders.push(new Header('Tipo Usuario','tipoUsuario'));   
+    return tableHeaders;
+  }
 }
