b6846ccbb8f8f4ee73f6b594a5448fbe33dbfd0a
Jean - Autofoz <jeanfelipe0610@gmail.com>
2023-11-03 13:55:54 -0300

    feat: back btn header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, Input, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -11,9 +12,14 @@ export class HeaderComponent implements OnInit {
   routerLink!: string;
   router = inject(Router)
   url!: string
-  constructor(){}
+  constructor(private location: Location){}
   ngOnInit(): void {
     this.url = this.router.url.split('/')[2]
     this.title = this.url.slice(0, 1).toUpperCase() + this.url.slice(1);
   }
+  back(){
+    console.log('ok')
+    this.location.back()
+  }
 }
+
