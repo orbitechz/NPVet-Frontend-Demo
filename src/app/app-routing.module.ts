65ac96227307e621f4542bd25234d83b17106993
Gustavo Piegat <gustavopiegat2004@gmail.com>
2023-11-03 22:45:37 -0300

    Criado exame Fisico cadastrar layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,17 +6,36 @@ import { UsuarioListarComponent } from './views/usuario/usuario-listar/usuario-l
 import { AnamneseListarComponent } from './views/anamnese/anamnese-listar/anamnese-listar.component';
 import { UsuarioDetailsComponent } from './views/usuario/usuario-details/usuario-details.component';
 import { UsuarioEditComponent } from './views/usuario/usuario-edit/usuario-edit.component';
+import { AnimalListComponent } from './views/animal/animal-list/animal-list.component';
+import { AnimalDetailsComponent } from './views/animal/animal-details/animal-details.component';
+import { ConsultaListComponent } from './views/consulta/consulta-list/consulta-list.component';
+import { IndexComponent } from './core/index/index.component';
+import { ExameListComponent } from './views/exame-fisico/exame-list/exame-list.component';
+import { ExameDetailsComponent } from './views/exame-fisico/exame-details/exame-details.component';
  
 
 const routes: Routes = [
-  {path: "tutores", component: TutorListComponent},
-  {path: "tutor/register", component: TutorDetailsComponent},
-  {path: "tutor/edit/:id", component: TutorDetailsComponent},
-  {path: "tutor/:id", component: TutorDetailsComponent},
-  {path: "usuarios", component: UsuarioListarComponent},
-  {path: "usuario/register", component: UsuarioDetailsComponent},
-  {path: "usuario/edit/:id", component: UsuarioEditComponent},
-  {path: "anamneses", component: AnamneseListarComponent},
+  { path: '', redirectTo: 'web', pathMatch: 'full' },
+  {
+    path: 'web',
+    component: IndexComponent,
+    children: [
+      { path: 'animal', component: AnimalListComponent },
+      { path: 'tutores', component: TutorListComponent },
+      { path: 'tutor/register', component: TutorDetailsComponent },
+      { path: 'tutor/edit/:id', component: TutorDetailsComponent },
+      { path: 'tutor/:id', component: TutorDetailsComponent },
+      { path: 'usuarios', component: UsuarioListarComponent },
+      { path: 'usuario/register', component: UsuarioDetailsComponent },
+      { path: 'usuario/edit/:id', component: UsuarioEditComponent },
+      { path: 'anamneses', component: AnamneseListarComponent },
+      { path: 'animais', component: AnimalListComponent },
+      { path: 'animal/cadastro', component: AnimalDetailsComponent },
+      { path: 'consultas', component: ConsultaListComponent },
+      { path: 'exame/register', component: ExameDetailsComponent },
+
+    ],
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { MenuComponent } from './core/menu/menu.component';
@@ -12,20 +11,27 @@ import { TutorListComponent } from './views/tutor/tutor-list/tutor-list.componen
 import { TutorDetailsComponent } from './views/tutor/tutor-details/tutor-details.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
- import { MensagemComponent } from './components/mensagem/mensagem.component';
+import { MensagemComponent } from './components/mensagem/mensagem.component';
 import { IconsModule } from './modules/icons.module';
 import { TableComponent } from './components/table/table.component';
 import { UsuarioService } from './services/usuario/usuario.service';
 import { NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
- import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { HeaderComponent } from './components/header/header.component';
 import { AnamneseListarComponent } from './views/anamnese/anamnese-listar/anamnese-listar.component';
 import { UsuarioListarComponent } from './views/usuario/usuario-listar/usuario-listar.component';
 import { UsuarioDetailsComponent } from './views/usuario/usuario-details/usuario-details.component';
 import { UsuarioEditComponent } from './views/usuario/usuario-edit/usuario-edit.component';
-
+import { AnimalListComponent } from './views/animal/animal-list/animal-list.component';
+import { AnimalDetailsComponent } from './views/animal/animal-details/animal-details.component';
+import { ConsultaListComponent } from './views/consulta/consulta-list/consulta-list.component';
+import { ConsultaDetailsComponent } from './views/consulta/consulta-details/consulta-details.component';
+import { RegisterBtnComponent } from './components/register-btn/register-btn.component';
+import { IndexComponent } from './core/index/index.component';
+import { ExameListComponent } from './views/exame-fisico/exame-list/exame-list.component';
+import { ExameDetailsComponent } from './views/exame-fisico/exame-details/exame-details.component';
 
 @NgModule({
   declarations: [
@@ -44,8 +50,16 @@ import { UsuarioEditComponent } from './views/usuario/usuario-edit/usuario-edit.
     AnamneseListarComponent,
     UsuarioDetailsComponent,
     UsuarioEditComponent,
-   ],
-  imports: [
+    AnimalListComponent,
+    AnimalDetailsComponent,
+    ConsultaListComponent,
+    ConsultaDetailsComponent,
+    RegisterBtnComponent,
+    IndexComponent,
+    ExameListComponent,
+    ExameDetailsComponent,
+  ],
+ imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
