e4114d114d8c9c566ad9b39a5a2a5cb63938f404
Jean Moschen - Pessoal <jeanfelipe0610@gmail.com>
2023-11-06 17:30:48 -0300

    feat: layout de consulta details

--- src/app/views/consulta/consulta-details/consulta-details.component.ts
+++ src/app/views/consulta/consulta-details/consulta-details.component.ts
@@ -1,13 +1,42 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, inject } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Header } from 'src/app/components/table/header';
+import { Animal } from 'src/app/models/animal/animal';
+import { Consulta } from 'src/app/models/consulta/consulta';
+import { Status } from 'src/app/models/enums/status';
 
 @Component({
   selector: 'app-consulta-details',
   templateUrl: './consulta-details.component.html',
-  styleUrls: ['./consulta-details.component.scss']
+  styleUrls: ['./consulta-details.component.scss'],
 })
 export class ConsultaDetailsComponent {
   @Output() confirmar = new EventEmitter<any>();
-  constructor(){
-    
+  keys = Object.keys;
+  statuses = Status;
+  isErro = false;
+  mensagem!: string;
+  disabled = false;
+  consulta = new Consulta();
+  modalService = inject(NgbModal);
+  constructor() {
+    this.consulta.animal = new Animal();
+  }
+
+  selecionarAnimal(template: any) {}
+
+  definirAnimal(animal: Animal) {
+    this.consulta.animal = animal;
+    this.modalService.dismissAll();
+  }
+  callAnimalHeaders(){
+    let tableHeaders : Header[] = [];
+    tableHeaders.push(new Header('Nome', 'nome'));
+    tableHeaders.push(new Header('Tutor', 'tutorId.nome'));
+    tableHeaders.push(new Header('Raça', 'raca'));
+    tableHeaders.push(new Header('Espécie', 'especie'));
+    tableHeaders.push(new Header('Sexo', 'sexo'));
+    tableHeaders.push(new Header('Data de Criação','createdAt'));    
+    return tableHeaders;
   }
 }
